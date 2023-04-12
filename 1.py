2b15d996b28226177e142d6ce901f1f8b5af8209
Reza Yazdani <mrrezayazdani@yahoo.com>
2023-04-12 17:39:07 +0330

    Update 1.py

--- 1.py
+++ 1.py
@@ -9,4 +9,23 @@
 
 # age = input("Sale tavaloted ro benevis: ")
 # print("shoma motavalede", 1402 - int(age) ,"hastid !")
-	
+
+# riazi = float(input("Nomre riazi : "))
+# fizik = float(input("Nomre fizik : "))
+# tarikh = float(input("Nomre tarikh : "))
+
+# print(type(riazi)) # no dade chie !
+
+# miangin = (riazi+fizik+tarikh) / 3
+
+# print("Miangin =", miangin)
+ # ------------------------------
+ # tamrin : 
+ # - adadi az karbar begirid
+ # - baghimande be 3,5,8 ra
+ # - chap konid :)
+
+adad = int(input("adad ra vared konid : "))
+print("Baghimande 3 =", adad%3)
+print("Baghimande 5 =", adad%5)
+print("Baghimande 8 =", adad%8)
