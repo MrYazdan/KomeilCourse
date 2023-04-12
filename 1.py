a9053c560326d2723d94e0dcc0ad50e1b0799039
Reza Yazdani <mrrezayazdani@yahoo.com>
2023-04-12 16:42:24 +0330

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,12 @@
+# Output
+# print("Salam")
+# print(12.5*5)
+
+# Input
+# first_name = input("Please enter your name: ")
+# last_name = input("Please enter your last name: ")
+# print(first_name, last_name)
+
+# age = input("Sale tavaloted ro benevis: ")
+# print("shoma motavalede", 1402 - int(age) ,"hastid !")
+	
