a73e0fba6ff5d979484e5091bba9b9e0d4e578d6
Reza Yazdani <mrrezayazdani@yahoo.com>
2023-05-11 10:46:17 +0330

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,66 @@
+# in ja gharare khorooji bedam
+# print("Salam be hame !")
+
+
+# name = input("name : ")
+# print("Esme shoma", name, "hast .")
+
+# int = -... -> +... = 1,2,-10
+# float =-.../... -> ... 1.4, 10, 2,4
+# bool = True, False
+# str = " any --- 3242 "
+
+# print(14//50)
+# print("14+50")
+
+# age = int(input("Please enter your age: "))
+# print("Your birthdate is", 2023-age
+
+# age = int(input("Please enter your age: "))
+
+# if age > 18 :
+#     print("Yes")
+# elif age == 18:
+#     print("Please Wait !")
+# else:
+#     print("No")
+
+# tamrin:
+# adadi az karbar begirid
+# agar zoj bood 'Yes'
+# agar fard bood 'No'
+# agar zefr bood '-'
+
+# zoj va fard:
+# num = int(input("> "))
+
+# if num == 0:
+#     print("----")
+# elif num % 2 == 0:
+#     print("Yes")
+# else:
+#     print("No")
+
+# tamrin:
+# adadi az karbar begirid
+# agar adad 2 raghami bood ba 10 tagsim shavad
+# dar gheir in soorat , angah adad ba 10 zarb shavad
+# adadi ke marahel bala ra gozarand:
+# agar zoj bood -> ba adad vared shode jam mikonim
+# agar fard bood -> ba adad vared shode kam mikonim
+
+num = float(input("> "))
+if 100 > num >= 10:
+    new_num = num / 10
+else:
+    new_num = num * 10
+
+
+if new_num % 2 == 0:
+    print(new_num + num)
+else:
+    print(num - new_num)
+
+
+
+
